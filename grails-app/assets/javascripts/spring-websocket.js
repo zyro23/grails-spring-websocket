4241ac4774d51fe4c92f87ce3b8536d7082d4f7e
zyro <zyro@zyro.net>
2014-02-11 21:48:48 +0100

    preparation for grails-2.4 resources _and_ asset-pipeline support. switched from minified js to normal js files as the resources-/asset-pipeline plugins both are capable of minification/uglifying/zipping

---
+++ grails-app/assets/javascripts/spring-websocket.js
@@ -0,0 +1,2 @@
+//=require sockjs
+//=require stomp
--- grails-app/conf/SpringWebsocketResources.groovy
+++ grails-app/conf/SpringWebsocketResources.groovy
@@ -1,6 +1,6 @@
 modules = {
 	"spring-websocket" {
-		resource url: "js/sockjs-0.3.4.min.js", disposition: "head"
-		resource url: "js/stomp.min.js", disposition: "head"
+		resource url: "js/sockjs.js", disposition: "head"
+		resource url: "js/stomp.js", disposition: "head"
 	}
 }
