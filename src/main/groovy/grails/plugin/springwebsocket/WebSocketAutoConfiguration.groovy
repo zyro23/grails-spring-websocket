5641d55022e8b9db5c2f38f8f7c3dd9a61511f16
zyro <zyro@zyro.net>
2025-08-11 14:54:57 +0200

    removed unnecessary autoconfig load order
    the default config is defined as a bean anyway (to allow overriding via groovy dsl)

--- src/main/groovy/grails/plugin/springwebsocket/WebSocketAutoConfiguration.groovy
+++ src/main/groovy/grails/plugin/springwebsocket/WebSocketAutoConfiguration.groovy
@@ -1,12 +1,11 @@
 package grails.plugin.springwebsocket
 
-import grails.boot.config.GrailsAutoConfiguration
 import groovy.transform.CompileStatic
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 
-@AutoConfiguration(after = GrailsAutoConfiguration)
+@AutoConfiguration
 @CompileStatic
 class WebSocketAutoConfiguration {
 
